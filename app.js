154a46d3aa92ed8d4109efc6f73fd22c71885a65
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-01-24 20:50:29 -0200

    Instalando EJS e refatorando projeto.
    EJS: Linguagem de modelagem para criação de páginas HTML utilizando JavaScript;

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs'); // ejs = motor de geração de views
+
 app.get('/', function(req, res){
 	res.send("<html><head><meta charset=\"UTF-8\"/></head><body>Portal de notícias</body></html>");
 });
 
 app.get('/tecnologia', function(req, res){
-	res.send('<html><head><meta charset=\"UTF-8\"/></head><body>Portal de Tecnologia</body></html>');
+	res.render('secao/tecnologia'); // não é necessário especificar extensão do arquivo "tecnologia" (ejs).
 });
 
 app.get('/moda', function(req, res){
