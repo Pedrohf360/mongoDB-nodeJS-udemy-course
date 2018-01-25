86ad5c1dbdc230a217e081fee03570be75143a56
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-01-25 17:32:07 -0200

    Criando conexão com BD e recuperando dados.
    Antes: instalação módulo que permite conexão com BD (mysql).
    
    MySQL commands used:
    mysql -u root -p
    password...
    
    create database portal_noticias;
    
    show databases;
    
    create table noticias(
    id_noticia int not null primary key auto_increment,
    titulo varchar(100),
    noticia text,
    data_criacao timestamp default current_timestamp);
    
    show tables;

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,21 @@
 module.exports = function(app) {
-		app.get('/noticias', function(req, res){
-		res.render('noticias/noticias');
+
+	app.get('/noticias', function(req, res)
+	{
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: 'admin',
+			database: 'portal_noticias'
+		}); // estrutura JSON (chave: 'valor')
+
+		connection.query('select * from noticias', function(error, result){
+			res.send(result);
+		}); // SQL: consulta em si;
+							// callback: o que vai ser feito após a  consulta ser realizada
+
+		//res.render('noticias/noticias');
 	});
 };
