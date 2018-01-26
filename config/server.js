1ca36a347840b2fbc11f956ada68e5b23217a045
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-01-25 23:20:07 -0200

    30. Refactoring do projeto prático portal de notícias com o Consign (routes).

--- config/server.js
+++ config/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 app.set('view engine', 'ejs'); // ejs = motor de geração de views
 app.set('views', './app/views');
 
+consign().include('app/routes').into(app);
+
 module.exports = app;
+
+// Como o server exporta uma função que é executada
+// pelo app, o caminho é buscada a partir do diretório
+// do app.js
