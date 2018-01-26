b133ee53cb0ea09f730ac24bb6e650e70d2bbddf
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-01-26 00:01:03 -0200

    32. Criando detalhes das notícias.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function() {	
+var connMySQL = function() {	
+		console.log('Conexão com BD foi estabelecida.');
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
 			password: 'admin',
 			database: 'portal_noticias'
 		}); // estrutura JSON (chave: 'valor')
+}
+
+module.exports = function () {
+	console.log('O autoload carregou o módulo de conexão com o BD');
+	return connMySQL;
 }
--- config/server.js
+++ config/server.js
@@ -5,7 +5,10 @@ var app = express();
 app.set('view engine', 'ejs'); // ejs = motor de geração de views
 app.set('views', './app/views');
 
-consign().include('app/routes').into(app);
+consign()
+	.include('app/routes')
+	.then('config/dbConnection.js')
+	.into(app);
 
 module.exports = app;
 
