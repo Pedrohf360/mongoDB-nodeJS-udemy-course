e8c1948b346b314148412aba25c9511cf6be1a70
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-02-06 12:14:14 -0200

    44. Refactoring do portal_noticias, com express-validator.
    Nomear o atributo "name" das tags de um formulário com nomes iguais aos das colunas de uma tabela, faz com que os dados sejam transferidos automaticamente, já que é via json file.
    Obs.: Função "isDate()", do express-validator, não existe mais! Logo, deveria ser implementada manualmente.

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,22 @@ module.exports = function(application) {
 	application.post('/noticias/salvar', function(req, res){
 		var noticia = req.body;
 
+		console.log(noticia);
+		// O primeiro parâmetro é a propriedade "name" das tags HTML
+		req.assert('titulo', 'Título é obrigatório!').notEmpty();
+		req.assert('resumo', 'Resumo é obrigatório!').notEmpty();
+		req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres.').len(10, 100);
+		req.assert('autor', 'Autor é obrigatório!').notEmpty();
+		req.assert('data_noticia', 'Data é obrigatória!').notEmpty();
+		req.assert('noticia', 'Notícia é obrigatório!').notEmpty();
+
+		var erros = req.validationErrors();
+
+		if(erros){
+			res.render('admin/form_add_noticia');
+			return;
+		}
+
 		var connection = application.config.dbConnection(); //conexao
 		var noticiasModel = new application.app.models.NoticiasDAO(connection); //model
 
