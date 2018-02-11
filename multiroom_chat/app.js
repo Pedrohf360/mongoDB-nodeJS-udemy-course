3cfc04f473d36dddc7d87f05455a6fac4a4ca5f2
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-02-11 21:44:19 -0200

    58-59. Preparando dep. e config. server.

---
+++ multiroom_chat/app.js
@@ -0,0 +1,9 @@
+/* import as configurações do servidor */
+
+var app = require('./config/server');
+
+/* parametrizar a porta de escuta */
+
+app.listen(80, function(){
+	console.log('Servidor online');
+})
