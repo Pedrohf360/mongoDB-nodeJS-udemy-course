d26df8d979565f3c347a6d9ff7fc0f237af46c2c
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-02-11 20:01:27 -0200

    52. Testar app via PC.
    -Obtendo 5 últimas notícias via método do módulo "NoticiasDAO".
    ***A partir de agora, codificar coisas deste curso APENAS via notebook!

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -11,8 +11,11 @@ NoticiasDAO.prototype.getNoticia = function(callback){
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
-	console.log('ronaldinho');
 	this._connection.query('insert into noticias set ? ', noticia, callback);
+}
+
+NoticiasDAO.prototype.get5UltimasNoticias = function(callback){
+	this._connection.query('select * from noticias order by data_criacao desc limit 5', callback);
 }
 
 module.exports = function(){
