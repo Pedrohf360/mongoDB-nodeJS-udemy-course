bcc8564ac0bc0ce9512997a9da6404252f7085b5
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-02-11 22:14:57 -0200

    61. Ajustando routes e controllers.

--- multiroom_chat/app/routes/chat.js
+++ multiroom_chat/app/routes/chat.js
@@ -1,11 +1,11 @@
 module.exports = function(application){
 	application.post('/chat', function(req, res){
-		res.render('chat');
+		application.app.controllers.chat.iniciaChat(application, req, res);
 	});
 }
 
 module.exports = function(application){
 	application.get('/chat', function(req, res){
-		res.render('chat');
+		application.app.controllers.chat.iniciaChat(application, req, res);
 	});
 }
