9044e86dd773366fc6321f29f85227f3f27345f4
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-02-11 22:59:25 -0200

    62. Recebendo e validando os dados do formulário de entrada.
    -Exibindo mensagens de erro(s) de preenchimento;
    -Detalhe curioso no controller: foi preciso informar {validacao : {}}.

--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.iniciaChat = function(application, req, res){
+
+	req.assert('apelido', 'Nome ou apelido é obrigatório!').notEmpty();
+	req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres!').len(3,15);
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render('index', {validacao : erros}); // Finaliza processamento.
+		return; // Por precaução.
+	}
+
 	res.render('chat');
 }
--- multiroom_chat/app/routes/chat.js
+++ multiroom_chat/app/routes/chat.js
@@ -2,9 +2,7 @@ module.exports = function(application){
 	application.post('/chat', function(req, res){
 		application.app.controllers.chat.iniciaChat(application, req, res);
 	});
-}
 
-module.exports = function(application){
 	application.get('/chat', function(req, res){
 		application.app.controllers.chat.iniciaChat(application, req, res);
 	});
