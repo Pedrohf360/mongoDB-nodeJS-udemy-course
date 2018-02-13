18febb719240392a45391162710991f2676b2d0c
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-02-13 10:08:43 -0200

    65-66. Refactoring - Integrando socket.io.
    Abrindo a conexão via websocket.

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -4,6 +4,20 @@ var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
 
-app.listen(80, function(){
+var server = app.listen(80, function(){
 	console.log('Servidor online');
 })
+
+var io = require('socket.io').listen(server);
+
+/* criar a conexão por websocket */
+
+io.on('connection', function(socket){
+	console.log('Usuário conectou!');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+
+	
+});
