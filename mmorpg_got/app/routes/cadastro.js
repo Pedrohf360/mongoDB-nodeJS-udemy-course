f362baaecc3d0297e036169a4ac679ac13a02705
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-05-28 07:30:51 -0300

    Fixing files types, index route, setting action and method to form component and giving a name to each of their components.

--- mmorpg_got/app/routes/cadastro.js
+++ mmorpg_got/app/routes/cadastro.js
@@ -2,4 +2,8 @@ module.exports = function (application) {
     application.get('/cadastro', function (req, res) {
         application.app.controllers.cadastro.cadastro(application, req, res);
     });
+
+    application.get('/cadastrar', function (req, res) {
+        application.app.controllers.cadastro.cadastro(application, req, res);
+    });
 }
--- mmorpg_got/app/routes/index.js
+++ mmorpg_got/app/routes/index.js
@@ -1,5 +1,5 @@
-module.exports = function(application){
-	application.get('/', function(req, res){
-		res.render('index');
+module.exports = function (application) {
+	application.get('/', function (req, res) {
+		application.app.controllers.index.index(application, req, res);
 	});
 }
