059ee016017fd4265f643195a0eb620611e798b6
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-07-06 19:45:00 -0300

    Working with express-session and adding node_modules to gitignore.

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function (application, req, res) {
     res.render('index');
 }
+
+module.exports.autenticar = function (application, req, res) {
+
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'O campo \"Usuário\" não pode ser vazio').notEmpty();
+    req.assert('senha', 'O campo \"Senha\" não pode ser vazio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render("index", {validacao: erros});
+        return;
+    }
+
+    
+}
